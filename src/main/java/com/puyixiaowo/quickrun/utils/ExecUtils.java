760a1d757d6f41f746de9c2d88771ad66aab721f
ysdxz207 <ysdxz207@qq.com>
2017-08-21 00:22:14 +0800

    fix can not open bat file lnk

--- src/main/java/com/puyixiaowo/quickrun/utils/ExecUtils.java
+++ src/main/java/com/puyixiaowo/quickrun/utils/ExecUtils.java
@@ -13,35 +13,17 @@ import java.io.IOException;
  */
 public class ExecUtils {
     public static boolean run(ShortCut shortCut) {
-        String command = "";
 
-        if (StringUtils.isBlank(shortCut.getTarget()) ||
-                !new File(shortCut.getTarget()).exists()) {
+        if (StringUtils.isBlank(shortCut.getLink()) ||
+                !new File(shortCut.getLink()).exists()) {
             Message.error(MainDialog.getInstance(), "运行“" + shortCut.getName() + "”失败，请确保目标路径可用。");
             return false;
         }
-        command = "cmd /c start /B " +
-                shortCut.getTarget().replaceAll(" ", "\" \"");
 
-        if (StringUtils.isNotBlank(shortCut.getCmdArgs())) {
-            command += " " + shortCut.getCmdArgs();
-        }
-
-//        try {
-//
-//            return run(command);
-//        } catch (IOException e) {
-//        }
-
-        try {
-            return startProgram(shortCut.getLink());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return false;
+        return startProgram(shortCut.getLink());
     }
 
-    public static boolean run(String command) throws IOException {
+    private static boolean runCmd(String command) throws IOException {
         Runtime rt = Runtime.getRuntime();
         Process p = rt.exec(command);
         return p != null;
@@ -54,7 +36,7 @@ public class ExecUtils {
      * @return
      * @throws IOException
      */
-    public static boolean startProgram(String programPath) throws IOException {
+    public static boolean startProgram(String programPath) {
         if (StringUtils.isNotBlank(programPath)) {
             try {
                 Desktop desktop = Desktop.getDesktop();
