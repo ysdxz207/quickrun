48f9e6742f898bc9bac63454d661ef536f481111
weishaoqiang <1061831336>
2018-02-01 16:49:21 +0800

    remove beautyeye

--- src/main/java/com/puyixiaowo/quickrun/main/Main.java
+++ src/main/java/com/puyixiaowo/quickrun/main/Main.java
@@ -2,30 +2,14 @@ package com.puyixiaowo.quickrun.main;
 
 import com.puyixiaowo.quickrun.dialog.MainDialog;
 import com.puyixiaowo.quickrun.utils.AppUtils;
-import com.puyixiaowo.quickrun.utils.Message;
-import org.jb2011.lnf.beautyeye.BeautyEyeLNFHelper;
-
-import javax.swing.*;
 
 /**
  * @author huangfeihong
  * @date 2016年12月25日 下午5:33:49
  */
 public class Main {
-	public static void main(String[] args) {
-		AppUtils.checkIfRunning();
-
-		try {
-			// 设置此开关量为false即表示关闭之，BeautyEye LNF中默认是true
-			BeautyEyeLNFHelper.translucencyAtFrameInactive = false;
-			// 设置本属性将改变窗口边框样式定义
-			BeautyEyeLNFHelper.frameBorderStyle = BeautyEyeLNFHelper.FrameBorderStyle.translucencyAppleLike;
-			org.jb2011.lnf.beautyeye.BeautyEyeLNFHelper.launchBeautyEyeLNF();
-			UIManager.put("RootPane.setupButtonVisible", false);// 关闭设置按钮
-		} catch (Exception e) {
-			Message.error(null, e.getMessage());
-		}
-
-		MainDialog.getInstance();
-	}
+    public static void main(String[] args) {
+        AppUtils.checkIfRunning();
+        MainDialog.getInstance();
+    }
 }
